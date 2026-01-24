0427a8a3c6364ced0d4fcecacc15eaf36ad794d8
Diogo Miranda <diogomsmiranda@gmail.com>
2026-01-24 20:40:10 +0000

    feat(utils): units module w/ paths

---
+++ src/utils/utils.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+DATA_DIR = Path("data")
+RAW_DIR = DATA_DIR / "raw"
+INTERIM_DIR = DATA_DIR / "interim"
+
+RAW_OPENDATA_CSV_DIR = RAW_DIR / "opendata_datasets_csv"
+RAW_OPENDATA_JSON_DIR = RAW_DIR / "opendata_datasets_json"
+
+INTERIM_OH_OPENDATA_CSV_DIR = INTERIM_DIR / "oh_opendata_datasets_csv"
+INTERIM_LOCATIONS_CSV = INTERIM_DIR / "locations.csv"
+INTERIM_STATIONS_CSV = INTERIM_DIR / "stations.csv"
+INTERIM_PORTS_CSV = INTERIM_DIR / "ports.csv"
